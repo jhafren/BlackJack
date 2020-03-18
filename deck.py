f125a57e677cb157f118ad5cf58378efc229fd79
haffe <haffe@LAPTOP-MTHRJET2>
2020-03-18 11:35:15 +0200

    Added reshuffling of the deck when it runs is empty

--- deck.py
+++ deck.py
@@ -7,7 +7,9 @@ class Deck:
 
     def __init__(self):
         self.cards = []
+        self.initialize_deck()
 
+    def initialize_deck(self):
         for suit in Deck.suits:
             for rank in range(1, 14):
                 self.cards.append(Card(rank, suit))
@@ -19,5 +21,7 @@ class Deck:
         try:
             return self.cards.pop(0)
         except IndexError:
-            print("Deck is empty")
-            return None
+            print("Deck is empty. Reshuffling...")
+            self.initialize_deck()
+            self.shuffle()
+            return self.draw_card()
